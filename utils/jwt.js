18eda6c70fea5c03a0bfe2b41148370e02217337
amine-amroussi <amineamro500@gmail.com>
2023-04-23 20:25:56 +0000

    Setup

--- utils/jwt.js
+++ utils/jwt.js
@@ -13,14 +13,16 @@ const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 const attachCookiesToResponse = ({ res, user }) => {
   const token = createJWT({ payload: user });
   const oneDay = 1000 * 60 * 60 * 24;
- 
+
   // attach cookie with response
   res.cookie("token", token, {
     httpOnly: true,
-    domain : "http://localhost:3000",
+    domain: "http://localhost:3000",
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === "production",
     signed: true,
+    sameSite: "strict",
+    path: "/",
   });
 };
 
