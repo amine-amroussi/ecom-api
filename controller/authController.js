b2aa7c49fa87373bbb851e4513bffd66c43c391c
amine-amroussi <amineamro500@gmail.com>
2023-04-24 05:00:21 +0000

    setup

--- controller/authController.js
+++ controller/authController.js
@@ -41,7 +41,7 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
   res.cookie("token", "logout", {
     httpOnly: true,
-    expires: new Date(Date.now() + '1d'),
+    expires: '1d',
     secure: true,
     signed: true,
     sameSite: "none",
