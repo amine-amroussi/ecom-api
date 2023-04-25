bb6517ab6b216a968188f181353f3eda27984074
amine-amroussi <amineamro500@gmail.com>
2023-04-25 23:14:36 +0000

    edit cookies

--- utils/jwt.js
+++ utils/jwt.js
@@ -21,7 +21,7 @@ const attachCookiesToResponse = ({ res, user }) => {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
-    
+    sameSite :"None"
   });
 };
 
